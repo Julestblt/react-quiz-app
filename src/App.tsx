6844933c68a824e908be3906036e09b63573f119
JULES <jules.thibault2002@gmail.com>
2022-05-30 20:54:57 +0200

    ✨ update tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Quiz, Home } from './pages'
 
 function App() {
     return (
-        <main className="min-h-screen bg-cover text-white flex flex-col bg-[url('./assets/background.png')]">
+        <main className="min-h-screen flex justify-center items-center text-white app-bg">
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/quiz" element={<Quiz />} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
     mode: 'jit',
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        colors: {
+            'dark-blue': '#2F527B',
+            'purple-opacity': 'rgba(96, 102, 208, 0.8)',
+            orange: '#F9A826',
+            white: '#FFFFFF',
+            dark: '#000000',
+        },
+        fontFamily: {
+            montserrat: ['Montserrat', 'sans-serif'],
+            poppins: ['Poppins', 'sans-serif'],
+        },
     },
     plugins: [],
 }
