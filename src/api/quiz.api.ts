419dbe4eb140ac7bbbad2cd609da5030ef6f122c
JULES <jules.thibault2002@gmail.com>
2022-05-30 22:52:03 +0200

    ✨ add basic api service

---
+++ src/api/quiz.api.ts
@@ -0,0 +1,36 @@
+import { buildUrl, QueryParams } from '../helpers/url'
+import axios from 'axios'
+import { AxiosRequestConfig } from 'axios'
+
+export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+
+const quizBaseUrl = process.env.REACT_APP_QUIZ_URL as string
+const quizApiKey = process.env.REACT_APP_QUIZ_API_KEY as string
+
+/**
+ * Allow to get the quiz api headers
+ * @returns The headers
+ */
+const getQuizHeaders = () => {
+    const headers = {
+        'X-Api-Key': quizApiKey,
+    }
+    return headers
+}
+
+const useQuizApi = async (
+    method: HttpMethod,
+    endpoint: string,
+    queryParams?: QueryParams
+): Promise<any> => {
+    const headers = getQuizHeaders()
+    const url = buildUrl(quizBaseUrl, endpoint, queryParams)
+    const config: AxiosRequestConfig = {
+        url,
+        headers,
+        method,
+    }
+    return await axios(config)
+}
+
+export { useQuizApi }
---
+++ src/api/use-quiz.api.ts
@@ -0,0 +1,64 @@
+import { useQuizApi } from './quiz.api'
+
+type QuizCategory =
+    | 'linux'
+    | 'devops'
+    | 'networking'
+    | 'programming'
+    | 'cloud'
+    | 'docker'
+    | 'kubernetes'
+
+type Difficulty = 'easy' | 'medium' | 'hard'
+
+type RandomQuizParams = {
+    category: QuizCategory
+    difficulty: Difficulty
+}
+
+interface QuizApi {
+    getRandomQuiz: (queryParams: RandomQuizParams) => Promise<RootObject>
+}
+
+const quizApi: QuizApi = {
+    getRandomQuiz: (queryParams) => useQuizApi('GET', 'questions', queryParams),
+}
+
+export interface Answers {
+    answer_a: string
+    answer_b: string
+    answer_c: string
+    answer_d: string
+    answer_e: string
+    answer_f?: any
+}
+
+export interface CorrectAnswers {
+    answer_a_correct: string
+    answer_b_correct: string
+    answer_c_correct: string
+    answer_d_correct: string
+    answer_e_correct: string
+    answer_f_correct: string
+}
+
+export interface Tag {
+    name: string
+}
+
+export interface RootObject {
+    id: number
+    question: string
+    description?: any
+    answers: Answers
+    multiple_correct_answers: string
+    correct_answers: CorrectAnswers
+    correct_answer: string
+    explanation?: any
+    tip?: any
+    tags: Tag[]
+    category: string
+    difficulty: string
+}
+
+export { quizApi }
