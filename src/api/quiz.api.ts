ccd0d210d4b37cf1cff09c8621e4eef0af719e29
JULES <jules.thibault2002@gmail.com>
2022-05-31 13:53:35 +0200

    ♻️ remove usage of axios

--- src/api/quiz.api.ts
+++ src/api/quiz.api.ts
@@ -1,11 +1,30 @@
 import { buildUrl, QueryParams } from '../helpers/url'
-import axios from 'axios'
-import { AxiosRequestConfig } from 'axios'
 
 export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
 
 const quizBaseUrl = process.env.REACT_APP_QUIZ_URL as string
 const quizApiKey = process.env.REACT_APP_QUIZ_API_KEY as string
+
+/**
+ * Just a bit a syntaxic sugar over the fetch API.
+ * Also allows to manage global loading state.
+ * @param method
+ * @param url
+ * @param config
+ * @returns
+ */
+const _fetch: (
+    method: HttpMethod,
+    url: string,
+    config: Partial<RequestInit>
+) => Promise<any> = async (method, url, config) => {
+    let response
+    const initConfig = { ...config, method }
+
+    response = await fetch(url, initConfig)
+
+    return response
+}
 
 /**
  * Allow to get the quiz api headers
@@ -25,12 +44,10 @@ const fetchQuizApi = async (
 ): Promise<any> => {
     const headers = getQuizHeaders()
     const url = buildUrl(quizBaseUrl, endpoint, queryParams)
-    const config: AxiosRequestConfig = {
-        url,
+    const config: Partial<RequestInit> = {
         headers,
-        method,
     }
-    return await axios(config)
+    return (await _fetch(method, url, config)).json()
 }
 
 export { fetchQuizApi }
