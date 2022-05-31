ea9e6fbd2978b63bd0f2eb23e04a7f4ada9160ef
JULES <jules.thibault2002@gmail.com>
2022-05-31 18:18:01 +0200

    ♻️ refacto <Button />

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,12 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
 
-export type ButtonVariant = 'primary' | 'secondary'
+export type ButtonVariant =
+    | 'primary'
+    | 'secondary'
+    | 'selected'
+    | 'success'
+    | 'error'
 
 interface ButtonProps {
     children?: string | React.ReactNode
@@ -9,31 +14,55 @@ interface ButtonProps {
     variant?: ButtonVariant
     disabled?: boolean
     restProps?: any
+    selected?: boolean
+    className?: string
+    success?: boolean
+    error?: boolean
 }
 
 const buttonVariants = {
-    default: 'rounded-lg py-2 px-4 font-poppins font-medium',
+    default:
+        'rounded-lg py-2 px-4 font-poppins font-medium flex justify-between items-center text-left text-lg',
     primary: 'bg-dark-blue',
-    secondary: 'border border-purple-opacity text-purple-opacity bg-white',
+    secondary:
+        'border border-purple-opacity text-purple-opacity bg-white hover:bg-purple-opacity hover:text-white hover:border-purple-opacity',
+    selected: 'border bg-orange text-white border-orange',
+    success: 'border bg-green border-green text-white',
+    error: 'border bg-red border-red text-white',
 }
 
 const Button = ({
     children,
     onClick,
     variant = 'primary',
+    className = '',
+    selected = false,
+    success = false,
+    error = false,
     ...restProps
 }: ButtonProps) => {
     const [buttonClass, setButtonClass] = useState<string>('')
 
+    const getButtonVariant = (): string => {
+        if (success) return buttonVariants['success']
+        else if (error) return buttonVariants['error']
+        else if (selected) return buttonVariants['selected']
+        else return buttonVariants[variant]
+    }
+
     useEffect(() => {
         setButtonClass(
-            [buttonVariants.default, buttonVariants[variant]].join(' ')
+            [buttonVariants.default, getButtonVariant(), className].join(' ')
         )
-    }, [variant])
+    }, [variant, selected, success, error])
 
     return (
         <button onClick={onClick} className={buttonClass} {...restProps}>
             {children}
+            {success && (
+                <span className="material-icons">check_circle_outline</span>
+            )}
+            {error && <span className="material-icons">highlight_off</span>}
         </button>
     )
 }
