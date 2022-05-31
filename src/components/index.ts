08a326491a0de529ff770f2300b68b4833e92d28
JULES <jules.thibault2002@gmail.com>
2022-05-31 15:13:31 +0200

    ✨ add <Loader />

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Card } from './card/Card'
 export { default as Button } from './button/Button'
 export { default as Select } from './select/Select'
+export { default as Loader } from './loader/Loader'
