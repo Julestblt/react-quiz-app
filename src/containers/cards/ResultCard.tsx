f165dc8f1edf6af7eaec66eaa7877c4251a375a7
JULES <jules.thibault2002@gmail.com>
2022-05-31 20:06:54 +0200

    💄 update styles

--- src/containers/cards/ResultCard.tsx
+++ src/containers/cards/ResultCard.tsx
@@ -1,27 +1,43 @@
-import { Card } from '../../components'
+import { Card, Button, Loader } from '../../components'
 import winner from '../../assets/svg/winner.svg'
+import { useState } from 'react'
 
 interface ResultCardProps {
     score: number
 }
 
 const ResultCard = ({ score }: ResultCardProps) => {
+    const [isLoaded, setIsLoaded] = useState<boolean>(false)
+
+    const newQuizHandler = () => (window.location.hash = '#')
+
+    const imageLoaded = () => setIsLoaded(true)
+
     return (
         <Card hideSvg>
-            <div className="px-8">
-                <img className="mx-auto" src={winner} alt="" />
-                <h1 className="text-dark-blue text-5xl font-poppins font-bold text-center">
-                    Results
-                </h1>
-                <p className="text-dark-blue font-poppins text-lg text-center">
-                    You got
-                    <span className="font-bold text-green text-2xl">
-                        {' '}
-                        {score}{' '}
-                    </span>
-                    correct answers
-                </p>
-            </div>
+            <img onLoad={imageLoaded} className="mx-auto" src={winner} alt="" />
+            {isLoaded ? (
+                <>
+                    <h1 className="text-dark-blue text-5xl font-poppins font-bold text-center my-10">
+                        Results
+                    </h1>
+                    <p className="text-dark-blue font-poppins text-lg text-center">
+                        You got
+                        <span className="font-bold text-green text-2xl">
+                            {' '}
+                            {score}{' '}
+                        </span>
+                        correct answers
+                    </p>
+                    <div className="flex justify-center mt-8">
+                        <Button onClick={newQuizHandler} variant="ghost">
+                            Try again
+                        </Button>
+                    </div>
+                </>
+            ) : (
+                <Loader />
+            )}
         </Card>
     )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -12,31 +12,29 @@ const Home = () => {
     }
 
     return (
-        <section className="font-poppins flex-auto flex justify-center -mt-16">
-            <Card titleCentered title="Welcome to my Quiz app">
-                <div className="grid grid-cols-2 gap-3 mt-3">
-                    <Select
-                        optionSelected={(option) => setCategory(option)}
-                        buttonTitle="Category"
-                        options={categoriesOptions}
-                    />
-                    <Select
-                        optionSelected={(option) => setDifficulty(option)}
-                        buttonTitle="Difficulty"
-                        options={difficultyOptions}
-                    />
-                </div>
-                <div className="text-center block mt-10">
-                    <Button
-                        onClick={quizHandler}
-                        variant="primary"
-                        disabled={!(category && difficulty)}
-                    >
-                        Start a new Quiz
-                    </Button>
-                </div>
-            </Card>
-        </section>
+        <Card titleCentered title="Welcome to my Quiz app">
+            <div className="grid grid-cols-2 gap-3 mt-3">
+                <Select
+                    optionSelected={(option) => setCategory(option)}
+                    buttonTitle="Category"
+                    options={categoriesOptions}
+                />
+                <Select
+                    optionSelected={(option) => setDifficulty(option)}
+                    buttonTitle="Difficulty"
+                    options={difficultyOptions}
+                />
+            </div>
+            <div className="flex justify-center mt-4">
+                <Button
+                    onClick={quizHandler}
+                    variant="primary"
+                    disabled={!(category && difficulty)}
+                >
+                    Start a new Quiz
+                </Button>
+            </div>
+        </Card>
     )
 }
 
