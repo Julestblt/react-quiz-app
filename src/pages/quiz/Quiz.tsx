120129d6e73b291a5ef5ab5c13302b8e94628e3d
JULES <jules.thibault2002@gmail.com>
2022-05-31 19:50:45 +0200

    ✨ add result workflow

--- src/pages/quiz/Quiz.tsx
+++ src/pages/quiz/Quiz.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { quizApi, QuizParams, RootObject } from '../../api/use-quiz.api'
 import { Button, Card, Loader } from '../../components'
+import { ResultCard } from '../../containers'
 import { getQueryParams } from '../../helpers/query-params'
 
 const { getRandomQuiz } = quizApi
@@ -12,6 +13,8 @@ const Quiz = () => {
     const [correctAnswers, setCorrectAnswers] = useState<string[]>([])
     const [showResult, setShowResult] = useState<boolean>(false)
     const [score, setScore] = useState<number>(0)
+    const [maxSelections, setMaxSelections] = useState<number>(0)
+    const [isQuizFinished, setIsQuizFinished] = useState<boolean>(false)
     const numberToAlphabet = ['A - ', 'B - ', 'C - ', 'D - ', 'E - ', 'F - ']
 
     const loadQuiz = async (params: QuizParams) => {
@@ -21,11 +24,13 @@ const Quiz = () => {
 
     const clickHandler = (id: number) => {
         if (!showResult) {
-            let maxSelections: number = quiz[currentQuiz].correct_answer
-                ? 1
-                : Object.values(quiz[currentQuiz].correct_answers).filter(
-                      (value) => value === 'true'
-                  ).length
+            setMaxSelections(
+                quiz[currentQuiz].correct_answer
+                    ? 1
+                    : Object.values(quiz[currentQuiz].correct_answers).filter(
+                          (value) => value === 'true'
+                      ).length
+            )
 
             if (
                 !selectedOptions.includes(id) &&
@@ -50,12 +55,24 @@ const Quiz = () => {
 
     const validateHandler = () => {
         setShowResult(true)
+        const isCorrect = selectedOptions.map((option) => {
+            return correctAnswers[option] === 'true'
+        })
+        if (
+            !isCorrect.includes(false) &&
+            maxSelections === selectedOptions.length
+        )
+            setScore(score + 1)
     }
 
     const nextQuestionHandler = () => {
-        setShowResult(false)
-        setSelectedOptions([])
-        setCurrentQuiz(currentQuiz + 1)
+        if (currentQuiz < quiz.length - 1) {
+            setShowResult(false)
+            setSelectedOptions([])
+            setCurrentQuiz(currentQuiz + 1)
+        } else {
+            setIsQuizFinished(true)
+        }
     }
 
     useEffect(() => {
@@ -63,58 +80,78 @@ const Quiz = () => {
         loadQuiz(queryParams)
     }, [])
 
-    return (
-        <Card title={quiz[currentQuiz]?.question}>
-            {quiz?.length > 0 ? (
-                <>
-                    <ul className="flex flex-col mt-3">
-                        {Object.values(quiz[currentQuiz]?.answers).map(
-                            (value, i) => (
-                                <li key={i} className="my-1">
-                                    {value && (
-                                        <Button
-                                            onClick={() => clickHandler(i)}
-                                            className="w-full"
-                                            variant="secondary"
-                                            selected={selectedOptions.includes(
-                                                i
+    if (!isQuizFinished)
+        return (
+            <>
+                <Card title={quiz[currentQuiz]?.question}>
+                    {quiz?.length > 0 ? (
+                        <>
+                            <ul className="flex flex-col mt-3">
+                                {Object.values(quiz[currentQuiz]?.answers).map(
+                                    (value, i) => (
+                                        <li key={i} className="my-1">
+                                            {value && (
+                                                <Button
+                                                    onClick={() =>
+                                                        clickHandler(i)
+                                                    }
+                                                    className="w-full"
+                                                    variant="secondary"
+                                                    selected={selectedOptions.includes(
+                                                        i
+                                                    )}
+                                                    success={
+                                                        showResult &&
+                                                        correctAnswers[i] ===
+                                                            'true'
+                                                    }
+                                                    error={
+                                                        showResult &&
+                                                        selectedOptions.includes(
+                                                            i
+                                                        ) &&
+                                                        correctAnswers[i] ===
+                                                            'false'
+                                                    }
+                                                >
+                                                    {numberToAlphabet[i]}
+                                                    {value}
+                                                </Button>
                                             )}
-                                            success={
-                                                showResult &&
-                                                correctAnswers[i] === 'true'
-                                            }
-                                            error={
-                                                showResult &&
-                                                selectedOptions.includes(i) &&
-                                                correctAnswers[i] === 'false'
-                                            }
-                                        >
-                                            {numberToAlphabet[i]}
-                                            {value}
-                                        </Button>
-                                    )}
-                                </li>
-                            )
-                        )}
-                    </ul>
-                    <span className="flex justify-center mt-2">
-                        {showResult ? (
-                            <Button onClick={nextQuestionHandler}>Next</Button>
-                        ) : (
-                            <Button onClick={validateHandler}>Validate</Button>
-                        )}
-                    </span>
-                </>
-            ) : (
-                <span className="flex justify-center">
-                    <Loader />
-                </span>
-            )}
-            <p className="text-center text-dark-blue mt-4 font-medium">
-                Question {currentQuiz + 1} of {quiz?.length}
-            </p>
-        </Card>
-    )
+                                        </li>
+                                    )
+                                )}
+                            </ul>
+                            <span className="flex justify-center mt-2">
+                                {showResult ? (
+                                    <Button onClick={nextQuestionHandler}>
+                                        Next
+                                    </Button>
+                                ) : (
+                                    <Button onClick={validateHandler}>
+                                        Validate
+                                    </Button>
+                                )}
+                            </span>
+                        </>
+                    ) : (
+                        <span className="flex justify-center">
+                            <Loader />
+                        </span>
+                    )}
+                    <p className="text-center text-dark-blue mt-4 font-medium">
+                        Question {currentQuiz + 1} of {quiz?.length}
+                    </p>
+                </Card>
+                <section className="bg-white w-min mx-auto text-center rounded-xl p-3 mt-4">
+                    <p className="font-medium text-dark-blue">Score</p>
+                    <p className="text-white bg-dark-blue rounded-xl font-bold">
+                        {score}
+                    </p>
+                </section>
+            </>
+        )
+    else return <ResultCard score={score} />
 }
 
 export default Quiz
